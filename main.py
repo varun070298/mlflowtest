31c5155a145fd79382c1062142cc72e3d7e7433f
Alpha_Panda <53207284+varun070298@users.noreply.github.com>
2022-02-14 13:38:54 +0100

    newt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import warnings
-import sys
 
 import numpy as np
 import pandas as pd
@@ -14,10 +13,12 @@ from scipy import stats
 import mlflow
 import mlflow.sklearn
 
+
 def eval_metrics(actual, pred,):
     mse = np.sqrt(mean_squared_error(actual, pred))
     r2 = r2_score(actual, pred)
     return mse, r2
+
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
@@ -32,25 +33,24 @@ if __name__ == "__main__":
 
     df.drop(['EstimatedScore'], axis=1, inplace=True)
 
-    a= df['No_4_Angle_Deviation'] * df['No_6_Angle_Deviation']
-    b= df['No_5_Angle_Deviation'] * df['No_7_Angle_Deviation']
-    c= df['No_8_Angle_Deviation'] * df['No_11_Angle_Deviation']
-    d= df['No_9_Angle_Deviation'] * df['No_12_Angle_Deviation']
-    e= df['No_10_Angle_Deviation'] * df['No_13_Angle_Deviation']
-    f= df['No_1_NASM_Deviation'] * df['No_2_NASM_Deviation']
-    g= df['No_4_NASM_Deviation'] * df['No_5_NASM_Deviation']
-    h= df['No_8_NASM_Deviation'] * df['No_9_NASM_Deviation']
-    i= df['No_11_NASM_Deviation'] * df['No_12_NASM_Deviation']
-    j= df['No_13_NASM_Deviation'] * df['No_14_NASM_Deviation']
-    k= df['No_15_NASM_Deviation'] * df['No_16_NASM_Deviation']
-    l= df['No_18_NASM_Deviation'] * df['No_19_NASM_Deviation']
+    a = df['No_4_Angle_Deviation'] * df['No_6_Angle_Deviation']
+    b = df['No_5_Angle_Deviation'] * df['No_7_Angle_Deviation']
+    c = df['No_8_Angle_Deviation'] * df['No_11_Angle_Deviation']
+    d = df['No_9_Angle_Deviation'] * df['No_12_Angle_Deviation']
+    e = df['No_10_Angle_Deviation'] * df['No_13_Angle_Deviation']
+    f = df['No_1_NASM_Deviation'] * df['No_2_NASM_Deviation']
+    g = df['No_4_NASM_Deviation'] * df['No_5_NASM_Deviation']
+    h = df['No_8_NASM_Deviation'] * df['No_9_NASM_Deviation']
+    i = df['No_11_NASM_Deviation'] * df['No_12_NASM_Deviation']
+    j = df['No_13_NASM_Deviation'] * df['No_14_NASM_Deviation']
+    k = df['No_15_NASM_Deviation'] * df['No_16_NASM_Deviation']
+    l = df['No_18_NASM_Deviation'] * df['No_19_NASM_Deviation']
 
+    df = df.assign(A4xA6=a, A5xA7=b, A8xA11=c, A9xA12=d, A10xA13=e, N1xN2=f, N4xN5=g, N8xN9=h, N11xN12=i, N13xN4=j,
+                   N15xN16=k, N18xN19=l)
 
-    df = df.assign(A4xA6=a,A5xA7=b,A8xA11=c,A9xA12=d,A10xA13=e,N1xN2=f,N4xN5=g,N8xN9=h,N11xN12=i,N13xN4=j,N15xN16=k,N18xN19=l)
-
-
-    X_train, X_test, Y_train, Y_test = train_test_split(df.iloc[:,1:53], df['AimoScore'], test_size = 0.1, random_state = 41)
-
+    X_train, X_test, Y_train, Y_test = train_test_split(df.iloc[:, 1:53], df['AimoScore'], test_size=0.1,
+                                                        random_state=41)
 
     with mlflow.start_run():
         LR = LinearRegression()
@@ -68,11 +68,7 @@ if __name__ == "__main__":
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-        # Model registry does not work with file store
         if tracking_url_type_store != "file":
-
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
             mlflow.sklearn.log_model(model, "model", registered_model_name="LinModel")
         else:
             mlflow.sklearn.log_model(model, "model")
